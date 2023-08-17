bea22e109fc3b39596ca21058208211ecfe69875
Alexander Deryabin <71517115+saskedr@users.noreply.github.com>
2023-08-17 17:06:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
-print('Hello, world!')
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def pasw(ctx, length=10):
+    await ctx.send(gen_pass(length))
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+bot.run("TOKEN")
